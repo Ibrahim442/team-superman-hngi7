c4e385256e880e302fb0e807a7f5ad1b17186b66
ibrahim <darboh112@gmail.com>
2020-06-03 14:21:21 +0000

    name: Ibrahim Daboh, id: HNG-01103 and email: Darboh112@gmail.com
    they above file is to be comitted to the script repository.

---
+++ scripts/ibrahim442.php
@@ -0,0 +1,11 @@
+console.log
+{
+file:  "myscript.py",
+output:  "Hello world, this is (ibrahim Daboh) with HNGi7 ID HNG-01103, using (javascript) for stage 2 task",
+name: "ibrahim Daboh",
+id: "HNG-01103",
+email:  darboh112@gmail.com.
+}
+
+
+
